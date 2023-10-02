fa0d3259c68256df6f79d45fbb7fb5f9ccc6d747
jmiarons <joelmiarons@protonmail.com>
2023-10-02 22:17:23 +0200

    Deleted depecrated setting

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -19,7 +19,6 @@ require("nvim-tree").setup {
     centralize_selection = false,
     width = 30,
     -- height = 30,
-    hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
